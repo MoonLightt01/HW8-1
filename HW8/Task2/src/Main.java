2b22cfdd92a31e1636d2e4748cefdb20943474c1
MoonLightt01 <byrman.slavik@gmail.com>
2023-06-04 00:44:37 +0300

    Completed hw 8

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        double[] weight = new double[10];
+        int summ = 0;
+        int peopleCount = 0;
+
+        for (int i = 0; i < weight.length; i++) {
+            Random random = new Random();
+            int num = 40 + random.nextInt(101) - 40;
+            weight[i] = num;
+            summ += weight[i];
+            if (weight[i] >= 60 && weight[i] <= 80) {
+                peopleCount++;
+            }
+        }
+
+
+        System.out.println("Средний вес: " + summ / weight.length);
+        System.out.println("Людей с весов (60-80):  " + peopleCount);
+
+
     }
 }
